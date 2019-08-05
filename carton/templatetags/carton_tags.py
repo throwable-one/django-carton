504b70f4e5dc02e5a2be6643260e03922127431b
Link <kazakevichilya@gmail.com>
2019-08-05 23:09:57 +0300

    migrate to new django

--- carton/templatetags/carton_tags.py
+++ carton/templatetags/carton_tags.py
@@ -3,7 +3,6 @@ from django import template
 from carton.cart import Cart
 from carton.settings import CART_TEMPLATE_TAG_NAME
 
-
 register = template.Library()
 
 
@@ -22,4 +21,5 @@ def get_cart(context, session_key=None, cart_class=Cart):
     request = context['request']
     return cart_class(request.session, session_key=session_key)
 
-register.assignment_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
+
+register.simple_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
